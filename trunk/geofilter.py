e47b7a5ed1c58fced6e9795a703c83d1c3c0f215
dominik.bartenstein <dominik.bartenstein@f0663f9a-1f2c-0410-911e-1bad8ebd410d>
2007-03-22 01:39:47 +0000

    damn slow :( my assumption is that it is because of the very complex polygon which represents the land of Vorarlberg.

--- trunk/geofilter.py
+++ trunk/geofilter.py
@@ -5,7 +5,38 @@
 #import shapelib
 import ogr
 import time
+import MySQLdb
 
+def createLineForFlight(flightid, step=1):
+    """ access geographical database info and create linestring """
+    database = 'flightdb'
+    user = 'flight'
+    password = 'flyaway'
+    
+    db = MySQLdb.connect(host = 'localhost', db = database, user = user, passwd = password)
+    cursor = db.cursor()
+    sql = "select latitude,longitude from flightdata where flightid=%i" % (flightid,)
+    cursor.execute(sql)
+    rs = cursor.fetchall()
+    
+    linestring = ogr.Geometry(ogr.wkbLineString)
+    linestring.SetCoordinateDimension(2)
+    #s = "POLYGON((";
+    #linestring.AddPoint_2D(23, 24)
+    
+    counter = 0
+    for longitude, latitude in rs:
+        if counter % step == 0:
+            #damn important performancewise! skip (0, 0) coordinates!
+            if longitude >0 and latitude > 0:
+                linestring.AddPoint_2D(float(latitude), float(longitude))
+        counter+=1 
+    print linestring
+    cursor.close()
+    print linestring.GetPointCount()
+    return linestring
+    
+    
 def main():
     SHAPEFILE = 'vlbg_wgs84_geogr.shp'
    
@@ -22,13 +53,20 @@ def main():
     geo = feature.GetGeometryRef()
 
  
-    wkt = 'POINT(9.96015 47.45753)'
+    #wkt = 'POINT(9.96015 47.45753)'
     #wkt = 'POINT(9.743 47.711)'
-    point = ogr.CreateGeometryFromWkt(wkt)
+    
     #should have same ref
 
     start = time.time()
-    print "within?: " + str(geo.Contains(point))
+    # 240733 yes ~470 seconds
+    # 240742 no
+    linestring = createLineForFlight(239390)
+    print "intersects?: " + str(geo.Intersect(linestring)) #benchmark: 474.821190119 seconds
+    # print "overlaps?: " + str(geo.Overlaps(linestring)) #benchmark: 466.047887087
+    # print "touches?: " + str(geo.Touches(linestring)) #benchmark: 468.383606911
+    #print "intersects?:" + str(geo.Intersect(polygon))
+    #print "within?: " + str(geo.Contains(point))
     print "duration: " + str(time.time() - start)
     
 if __name__ == '__main__':
