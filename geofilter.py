91c72fec342ce6f4192bd35ccac9369a579e31e9
dominik.bartenstein <dominik.bartenstein@f0663f9a-1f2c-0410-911e-1bad8ebd410d>
2007-03-24 15:06:20 +0000

    started refactoring

--- geofilter.py
+++ geofilter.py
@@ -6,80 +6,116 @@
 import ogr
 import time
 import MySQLdb
+import logging
+SHAPEFILE = 'vlbg_wgs84_geogr.shp'
 
-def createLineForFlight(flightid, step=1):
-    """ access geographical database info and create linestring """
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+
+class FlightAnalyzer:
+    ''' geographical analyzer of flights '''
+    
+    host = 'localhost'
     database = 'flightdb'
     user = 'flight'
     password = 'flyaway'
     
-    db = MySQLdb.connect(host = 'localhost', db = database, user = user, passwd = password)
-    cursor = db.cursor()
-    sql = "select latitude,longitude from flightdata where flightid=%i" % (flightid,)
-    cursor.execute(sql)
-    rs = cursor.fetchall()
+    def __init__(self):
+        self.db = MySQLdb.connect(host = self.host, db = self.database, user = self.user, passwd = self.password)
+
+    def processFlight(self, flightid):
+        ''' '''
+        isIntersecting = self.checkFlight(flightid)
+        self.tagFlight(flightid, isIntersecting)
     
-    linestring = ogr.Geometry(ogr.wkbLineString)
-    linestring.SetCoordinateDimension(2)
-    #s = "POLYGON((";
-    #linestring.AddPoint_2D(23, 24)
+    def checkFlight(self, flightid):
+        ''' check if flight went over Vorarlberg '''
+ 
+        # SHAPELIB approach (unused atm)
+        # shapefile = shapelib.open(SHAPEFILE, 'r')
+        # numshapes, shapetype, mins, maxs = shapefile.info()
+        # print "number of shapes: %i\nshapetype: %i\nmins: %s\nmaxs: %s" % (numshapes, shapetype, mins, maxs)
+        
+        # there is only one shape in the file, so we access it via index 0
+        # shape = shapefile.read_object(0)
+        
+        src = ogr.Open(SHAPEFILE)
+        layer = src.GetLayer()
+        feature = layer.GetNextFeature()
+        geo = feature.GetGeometryRef()
     
-    counter = 0
-    for longitude, latitude in rs:
-        if counter % step == 0:
-            #damn important performancewise! skip (0, 0) coordinates!
-            if longitude >0 and latitude > 0:
-                linestring.AddPoint_2D(float(latitude), float(longitude))
-        counter+=1 
-    print linestring
-    cursor.close()
-    print linestring.GetPointCount()
-    return linestring
+        # benchmark
+        start = time.time()
+        
+        # test runs: manually tracked some flights and 
+        # checked visually whether they crossed vorarlberger air space
+        # 240733 yes ~470 seconds
+        # 240742 no
+        # 243494 dunno
+        # 243663 yes
+        # 243666 no
+        # 243689 yes
+        # 243824 yes
+        # 244673 no 
+        # 244676 no
+        #print "distance?: " + str(geo.Distance(linestring)) #benchmark: 466.047887087
+        #print "overlaps?: " + str(geo.Overlaps(linestring)) #benchmark: 466.047887087
+        #print "touches?: " + str(geo.Touches(linestring)) #benchmark: 468.383606911
+        #print "intersects?:" + str(geo.Intersect(polygon))
+        #print "within?: " + str(geo.Contains(point))
+        
+        linestring = self.createFlightLine(flightid)
+        isIntersecting = geo.Intersect(linestring)
+        
+        logging.info("flight #%i over vlbg?: %i" %(flightid, isIntersecting))
+        logging.info("benchmark: %d seconds" % (time.time() - start))
+        return isIntersecting
+    
+    def createFlightLine(self, flightid, step=1):
+        """ access geographical database info and create linestring """
+        cursor = self.db.cursor()
+        sql = "select latitude,longitude from flightdata where flightid=%i" % (flightid,)
+        cursor.execute(sql)
+        rs = cursor.fetchall()
+        
+        linestring = ogr.Geometry(ogr.wkbLineString)
+        linestring.SetCoordinateDimension(2)
+        
+        counter = 0
+        for longitude, latitude in rs:
+            if counter % step == 0:
+                #damn important performancewise! skip (0, 0) coordinates!
+                if longitude >0 and latitude > 0:
+                    linestring.AddPoint_2D(float(latitude), float(longitude))
+            counter+=1 
+        logging.debug(linestring)
+        cursor.close()
+        logging.debug("pointcount: %i" % linestring.GetPointCount())
+        return linestring
+    
+    def tagFlight(self, flightid, overVlbg=0):
+        ''' set flag for flight '''
+        
+        cursor = self.db.cursor()
+        sql = "UPDATE flights SET overvlbg=%i WHERE id=%i" %(overVlbg, flightid)
+        logging.info(sql)
+        cursor.execute(sql)
+        cursor.close()
+        
     
     
 def main():
-    SHAPEFILE = 'vlbg_wgs84_geogr.shp'
-   
-    # shapefile = shapelib.open(SHAPEFILE, 'r')
-    # numshapes, shapetype, mins, maxs = shapefile.info()
-    # print "number of shapes: %i\nshapetype: %i\nmins: %s\nmaxs: %s" % (numshapes, shapetype, mins, maxs)
-    
-    # there is only one shape in the file, so we access it via index 0
-    # shape = shapefile.read_object(0)
-    
-    src = ogr.Open(SHAPEFILE)
-    layer = src.GetLayer()
-    feature = layer.GetNextFeature()
-    geo = feature.GetGeometryRef()
-
- 
-    #wkt = 'POINT(9.96015 47.45753)'
-    #wkt = 'POINT(9.743 47.711)'
-    
-    #should have same ref
-
-    start = time.time()
-    # test runs: manually tracked some flights and 
-    # checked visually whether they crossed vorarlberger air space
-    # 240733 yes ~470 seconds
-    # 240742 no
-    # 243494 dunno
-    # 243663 yes
-    # 243666 no
-    # 243689 yes
-    # 243824 yes
-    # 244673 no 
-    # 244676 no
-    linestring = createLineForFlight(244676)
-    #print "distance?: " + str(geo.Distance(linestring)) #benchmark: 466.047887087
-    print "intersects?: " + str(geo.Intersect(linestring)) #benchmark: 474.821190119 seconds
-    #print "overlaps?: " + str(geo.Overlaps(linestring)) #benchmark: 466.047887087
-    #print "touches?: " + str(geo.Touches(linestring)) #benchmark: 468.383606911
-    #print "intersects?:" + str(geo.Intersect(polygon))
-    #print "within?: " + str(geo.Contains(point))
-    print "duration: " + str(time.time() - start)
+    analyzer = FlightAnalyzer()
+    cursor = analyzer.db.cursor()
+    sql = "SELECT DISTINCT flightid FROM flightdata WHERE time BETWEEN '2007-03-24 13:00' AND '2007-03-24 14:00'"
+    cursor.execute(sql)
+    rs = cursor.fetchall()
+    analyzer.processFlight(245823)
+    for record in rs:
+        flightid= record[0]
+        #print flightid
+        #analyzer.processFlight(flightid) #245680
     
 if __name__ == '__main__':
     main()
-# search for flights which are already out of sight 
-# select count(*) from flightdata where flightid=244678 and time>NOW()-1000;
+    # search for flights which are already out of sight 
+    # select count(*) from flightdata where flightid=244678 and time>NOW()-1000;
