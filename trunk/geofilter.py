a0a1f4d48d75246d08d780a418f70381a102ed8f
dominik.bartenstein <dominik.bartenstein@f0663f9a-1f2c-0410-911e-1bad8ebd410d>
2007-03-28 00:53:59 +0000

    major cleanups: - moved sql-ideas to wiki - removed shapelib comments - removed unnecessary comments

--- trunk/geofilter.py
+++ trunk/geofilter.py
@@ -6,11 +6,13 @@ import ogr
 import time
 import MySQLdb
 import logging
+
 # found this very nice online tool http://www.mapshaper.org to simplify the map
-SHAPEFILE = 'vlbg_wgs84_geogr.shp'
-SHAPEFILE = 'vlbg_wgs84_simplified.shp'
-SHAPEFILE = 'vlbg_wgs84_visvalingam_75.shp'
-#SHAPEFILE = 'vlbg_wgs84_douglas_14.shp'
+# now the map should be available to the public as well
+#SHAPEFILE = 'vlbg_wgs84_geogr.shp'
+#SHAPEFILE = 'vlbg_wgs84_simplified.shp'
+#SHAPEFILE = 'vlbg_wgs84_visvalingam_75.shp'
+SHAPEFILE = 'vlbg_wgs84_douglas_14.shp'
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
 
@@ -37,48 +39,22 @@ class FlightAnalyzer:
     
     def checkFlight(self, flightid):
         ''' check if flight went over Vorarlberg '''
- 
-        # SHAPELIB approach (unused atm)
-        # shapefile = shapelib.open(SHAPEFILE, 'r')
-        # numshapes, shapetype, mins, maxs = shapefile.info()
-        # print "number of shapes: %i\nshapetype: %i\nmins: %s\nmaxs: %s" % (numshapes, shapetype, mins, maxs)
-        
-        # there is only one shape in the file, so we access it via index 0
-        # shape = shapefile.read_object(0)
-        
-      
-    
+
         # benchmark
         start = time.time()
-        
-        # test runs: manually tracked some flights and 
-        # checked visually whether they crossed vorarlberger air space
-        # 240733 yes ~470 seconds
-        # 240742 no
-        # 243494 dunno
-        # 243663 yes
-        # 243666 no
-        # 243689 yes
-        # 243824 yes
-        # 244673 no 
-        # 244676 no
-        #print "distance?: " + str(geo.Distance(linestring)) #benchmark: 466.047887087
-        #print "overlaps?: " + str(geo.Overlaps(linestring)) #benchmark: 466.047887087
-        #print "touches?: " + str(geo.Touches(linestring)) #benchmark: 468.383606911
-        #print "intersects?:" + str(geo.Intersect(polygon))
-        #print "within?: " + str(geo.Contains(point))
-        
+    
+        # create linestring from flight route and check if it intersects
         linestring = self.createFlightLine(flightid)
         isIntersecting = self.geometry.Intersect(linestring)
         
-        logging.info("flight #%i over vlbg?: %i" %(flightid, isIntersecting))
-        logging.info("benchmark: %d seconds" % (time.time() - start))
+        logging.info("flight #%i intersecting?: %i" %(flightid, isIntersecting))
+        logging.info("benchmark: %f seconds" % (time.time() - start))
         return isIntersecting
     
     def createFlightLine(self, flightid, step=1):
         """ access geographical database info and create linestring """
         cursor = self.db.cursor()
-        sql = "select latitude,longitude from flightdata where flightid=%i" % (flightid,)
+        sql = "select latitude,longitude from flightdata where flightid=%i" %flightid
         cursor.execute(sql)
         rs = cursor.fetchall()
         
@@ -92,13 +68,12 @@ class FlightAnalyzer:
                 if longitude >0 and latitude > 0:
                     linestring.AddPoint_2D(float(latitude), float(longitude))
             counter+=1 
-        #logging.debug(linestring)
         cursor.close()
         logging.debug("pointcount: %i" % linestring.GetPointCount())
         return linestring
     
     def tagFlight(self, flightid, overVlbg=0):
-        ''' set flag for flight '''
+        ''' set flag for flight in db '''
         
         cursor = self.db.cursor()
         sql = "UPDATE flights SET overvlbg=%i WHERE id=%i" %(overVlbg, flightid)
@@ -114,15 +89,11 @@ def main():
     sql = "SELECT DISTINCT flightid FROM flightdata" #WHERE time BETWEEN '2007-03-28 00:00' AND '2007-03-29 23:59'"
     cursor.execute(sql)
     rs = cursor.fetchall()
-    #analyzer.processFlight(247785) #THA931 very close touching?
+  
+    # loop over all flights retrieved by sql statement and process it
     for record in rs:
         flightid= record[0]
         analyzer.processFlight(flightid) #245680
     
 if __name__ == '__main__':
     main()
-    # search for flights which are already out of sight 
-    # select count(*) from flightdata where flightid=244678 and time>NOW()-1000;
-
-    # sql-statement for finding all flights not yet geographically classified
-    # SELECT callsign, hexident FROM flights INNER JOIN aircrafts ON flights.aircraftid=aircrafts.id WHERE flights.overVlbg IS NULL AND flights.id in (SELECT DISTINCT flightid FROM flightdata WHERE time BETWEEN '2007-03-24 14:00' AND '2007-03-24 18:00');
