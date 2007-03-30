792d6f2ae7b9689e69d22793d23d0d57eb815888
dominik.bartenstein <dominik.bartenstein@f0663f9a-1f2c-0410-911e-1bad8ebd410d>
2007-03-30 00:45:58 +0000

    added column altitude to table flightdata, type UNSIGNED SMALLINT

--- dbstructure.sql
+++ dbstructure.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: flightdb
 -- ------------------------------------------------------
--- Server version	5.0.24a-Debian_9ubuntu0.1-log
+-- Server version	5.0.22-Debian_0ubuntu6.06.3-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -27,7 +27,7 @@ CREATE TABLE `airbornevelocitymessage` (
   `verticalrate` smallint(6) default NULL,
   `track` smallint(5) unsigned default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=innoDB AUTO_INCREMENT=3092307 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `aircrafts`
@@ -38,7 +38,7 @@ CREATE TABLE `aircrafts` (
   `id` bigint(20) unsigned NOT NULL,
   `hexident` varchar(6) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=innoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `flightdata`
@@ -53,9 +53,10 @@ CREATE TABLE `flightdata` (
   `time` datetime default NULL,
   `time_ms` smallint(5) unsigned default NULL,
   `transmissiontype` tinyint(3) unsigned default NULL,
+  `altitude` smallint(5) unsigned NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `time` (`time`)
-) ENGINE=innoDB AUTO_INCREMENT=2938141 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `flights`
@@ -72,7 +73,7 @@ CREATE TABLE `flights` (
   KEY `aircraftid` (`aircraftid`),
   KEY `overvlbg` (`overvlbg`),
   KEY `ts` (`ts`)
-) ENGINE=innoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `kexi__blobs`
@@ -87,7 +88,7 @@ CREATE TABLE `kexi__blobs` (
   `o_mime` varchar(200) NOT NULL,
   `o_folder_id` int(10) unsigned default NULL,
   PRIMARY KEY  (`o_id`)
-) ENGINE=innoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `kexi__db`
@@ -97,7 +98,7 @@ DROP TABLE IF EXISTS `kexi__db`;
 CREATE TABLE `kexi__db` (
   `db_property` varchar(32) default NULL,
   `db_value` longtext
-) ENGINE=innoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `kexi__fields`
@@ -116,7 +117,7 @@ CREATE TABLE `kexi__fields` (
   `f_order` int(11) default NULL,
   `f_caption` varchar(200) default NULL,
   `f_help` longtext
-) ENGINE=innoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `kexi__objectdata`
@@ -127,7 +128,7 @@ CREATE TABLE `kexi__objectdata` (
   `o_id` int(10) unsigned NOT NULL,
   `o_data` blob,
   `o_sub_id` varchar(200) default NULL
-) ENGINE=innoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `kexi__objects`
@@ -141,7 +142,7 @@ CREATE TABLE `kexi__objects` (
   `o_caption` varchar(200) default NULL,
   `o_desc` longtext,
   PRIMARY KEY  (`o_id`)
-) ENGINE=innoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `kexi__parts`
@@ -154,7 +155,7 @@ CREATE TABLE `kexi__parts` (
   `p_mime` varchar(200) default NULL,
   `p_url` varchar(200) default NULL,
   PRIMARY KEY  (`p_id`)
-) ENGINE=innoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
