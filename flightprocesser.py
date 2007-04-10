f70e97c74b744354c8da447a0c09b3bcc4f6017e
dominik.bartenstein <dominik.bartenstein@f0663f9a-1f2c-0410-911e-1bad8ebd410d>
2007-04-10 22:12:33 +0000

    the flightprocessor is responsible for: - merging callsign flickering flights - geographically classifying the flights
    the flightprocessor will substitute geofilter.py

---
+++ flightprocesser.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+# Applying geo-operations to flights
+# Copyright (GPL) 2007 Dominik Bartenstein <db@wahuu.at>
+import ogr
+import time
+import MySQLdb
+import logging
+
+# found this very nice online tool http://www.mapshaper.org to simplify the map
+# the simpliefied map is made public 
+SHAPEFILE = 'data/vlbg_wgs84_douglas_14.shp'
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+
+class FlightAnalyzer:
+    ''' geographical analyzer of flights '''
+    
+    host = 'localhost'
+    database = 'flightdb'
+    user = 'flight'
+    password = 'flyaway'
+    
+    def __init__(self):
+        self.db = MySQLdb.connect(host = self.host, db = self.database, user = self.user, passwd = self.password)
+        
+        self.src = ogr.Open(SHAPEFILE)
+        self.layer = self.src.GetLayer()
+        self.feature = self.layer.GetNextFeature()
+        self.geometry = self.feature.GetGeometryRef()
+    
+    def mergeFlights(self):
+        ''' fix callsign flickering troubles '''
+        
+        cursor = self.db.cursor()
+        sql = "SELECT DISTINCT a.ts, aircrafts.hexident, a.callsign, a.id FROM flights AS a, flights as b INNER JOIN aircrafts ON aircraftid = aircrafts.id WHERE a.aircraftid=b.aircraftid AND a.id != b.id AND b.overVlbg IS NOT NULL AND a.overVlbg IS NOT NULL AND timestampdiff(MINUTE, a.ts, b.ts) BETWEEN 0 AND 45 ORDER BY a.ts"
+        cursor.execute(sql)
+        rs = cursor.fetchall()
+        
+        # collect flights which belong togeter
+        # format: HEXIDENT2: [CALLSIGN1, CALLSIGN2, ...], HEXIDENT2
+        hextable = {}
+        for record in rs:
+            hexident = record[1]
+            callsign = record[2]
+            flightid = record[3]
+            pairs = hextable.get(hexident, [])
+            pairs.append((flightid, callsign))
+            hextable[hexident] = pairs
+        logging.info(hextable)
+        
+        for key in hextable.keys():
+            pairs = hextable.get(key)
+            callsigns = [cs for flightid, cs in pairs]
+            logging.info(callsigns)
+            freq = [(a, callsigns.count(a)) for a in set(callsigns)]
+            freq.sort(lambda a, b: cmp(b[1], a[1]))
+            logging.info(freq)
+            callsign = freq[0][0]
+            logging.info("callsign: %s" %callsign)
+            
+            mergedflightids = [flightid for flightid, cs in pairs]
+            mainflightid = mergedflightids[0]
+            mergedflightids.remove(mainflightid)
+            
+            logging.info("main flightid: %s" %mainflightid)
+            logging.info("merged flightids: %s" %mergedflightids)
+            
+    def processFlight(self, flightid):
+        ''' 1. check flight + 2. tag flight'''
+        isIntersecting = self.checkFlight(flightid)
+        self.tagFlight(flightid, isIntersecting)
+    
+    def checkFlight(self, flightid):
+        ''' check if flight went over Vorarlberg '''
+
+        # benchmark
+        start = time.time()
+    
+        # create linestring from flight route and check if it intersects
+        linestring = self.createFlightLine(flightid)
+        isIntersecting = self.geometry.Intersect(linestring)
+        
+        logging.info("flight #%i intersecting?: %i" %(flightid, isIntersecting))
+        logging.info("benchmark: %f seconds" % (time.time() - start))
+        return isIntersecting
+    
+    def createFlightLine(self, flightid):
+        """ access geographical database info and create linestring """
+        
+        cursor = self.db.cursor()
+        sql = "SELECT latitude,longitude FROM flightdata WHERE flightid=%i" %flightid
+        cursor.execute(sql)
+        rs = cursor.fetchall()
+        
+        # create empty 2-dimensional linestring-object representing a flight route tracked by the SBS-1
+        linestring = ogr.Geometry(ogr.wkbLineString)
+        linestring.SetCoordinateDimension(2)
+        for longitude, latitude in rs:
+            #pretty important: skip unnecessary (0, 0) coordinates!
+            if longitude > 0 and latitude > 0:
+                linestring.AddPoint_2D(float(latitude), float(longitude))
+        cursor.close()
+        
+        logging.info("pointcount: %i" % linestring.GetPointCount())
+        return linestring
+    
+    def tagFlight(self, flightid, overVlbg=0):
+        ''' set flag for flight in db '''
+        
+        cursor = self.db.cursor()
+        sql = "UPDATE flights SET overvlbg=%i WHERE id=%i" %(overVlbg, flightid)
+        logging.info(sql)
+        cursor.execute(sql)
+        self.db.commit()
+        cursor.close()
+    
+def main():
+    logging.info("### GEOFILTER started")
+    analyzer = FlightAnalyzer()
+    cursor = analyzer.db.cursor()
+    # grab all flights not yet classified geographically
+    # and not currently in progress
+    # NOTE: new flights appear in realtime, other messages are 5 minutes delayed
+    # -> 1. only check flights which were added more than 6 minutes ago
+    # -> 2. only check flights where the most recent flightdata is older than 6 minutes
+    # -> 3. only check flights where all identical flights with different ids (callsign flickering) are older than 6 minutes
+    analyzer.mergeFlights()
+    sql = "SELECT id FROM flights WHERE overVlbg IS NULL AND ts < NOW()-INTERVAL 6 MINUTE AND id NOT IN (SELECT DISTINCT flightid FROM flightdata WHERE time  > NOW()-INTERVAL 6 MINUTE) AND flights.aircraftid NOT in (SELECT DISTINCT aircraftid FROM flights INNER JOIN flightdata ON flights.id = flightdata.flightid AND ts > NOW()-INTERVAL 6 MINUTE)"
+    cursor.execute(sql)
+    rs = cursor.fetchall()
+ 
+    # loop over all flights and check'em 
+   # for record in rs:
+    #    flightid = record[0]
+     #   analyzer.processFlight(flightid)
+
+    cursor.close() 
+    logging.info("### GEOFILTER finished")
+ 
+if __name__ == '__main__':
+    main()
