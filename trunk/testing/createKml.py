726992713a7ff4701abbbb63d3e6448dd93b9822
dominik.bartenstein <dominik.bartenstein@f0663f9a-1f2c-0410-911e-1bad8ebd410d>
2007-05-15 16:11:09 +0000

    added configuration option overVlbg

--- trunk/testing/createKml.py
+++ trunk/testing/createKml.py
@@ -5,6 +5,8 @@ import MySQLdb
 import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+FILENAME = 'flights.kml'
+overVlbg = (0, 1)
 
 class KMLCreator:
     ''' creator of kml files '''
@@ -21,122 +23,121 @@ class KMLCreator:
     def createFile(self):
         ''' create kml file! '''
        
-        sql = self.basesql + " AND flights.overVlbg=1" 
-        cursor = self.db.cursor()
-        cursor.execute(sql)
-        rs = cursor.fetchall()
-        # loop over all flights and check'em 
-        placemark = ""
-        for record in rs:
-            flightid = record[0]
-            callsign = record[1]
-            hexident = record[2]
-            ts = record[3]
-      
-            logging.log(logging.INFO, flightid)
-            sql = "SELECT longitude, latitude FROM flightdata WHERE flightid=%i" %flightid
-            logging.info(sql)
-            cursor2 = self.db.cursor()
-            cursor2.execute(sql)
-            rs2 = cursor2.fetchall()
-            
-            # only use 10% of data!
-            c = 0
-            SKIP = 30
-            length = len(rs2)
-            SKIP = length / 25
-            coordinateinfo = ""     
-            clist = []
-            logging.info(SKIP)
-            for data in rs2:
-                longitude= data[0]
-                latitude = data[1]
-                # damn, some flights have pretty strange GPS-info! define a value range
-                if latitude > 40 and latitude < 50 and longitude > 8 and longitude < 15: 
-                    if c % SKIP == 0:
-                        coordinateinfo += "%f,%f,20000 \n" %(longitude, latitude)
-                        clist.append( (longitude, latitude) )
-                    c += 1
-            cursor2.close()
-            placemark += """<Placemark>
-      <name>-</name>
-      <description>-</description>
-      <styleUrl>#red</styleUrl>
-      <LineString>
-        <extrude>0</extrude>
-        <tessellate>1</tessellate>
-        <altitudeMode>absolute</altitudeMode>
-        <coordinates>%s</coordinates>
-      </LineString>
-    </Placemark>""" %coordinateinfo
-        
-            # append location icon
-            long, lat = clist[len(clist)/2]
-            placemark += """<Placemark>
-            <name>flight: %s</name>
-            <description>aircraft: %s  spotted at: %s</description>
-            <styleUrl>#normalPlacemark</styleUrl>
-            <Point>
-            <coordinates>%f,%f,20000</coordinates>
-            </Point>
-            </Placemark>
-        """ % (callsign, hexident, ts, long, lat)
-        
-        cursor.close() 
-    
-        cursor = self.db.cursor()
-        sql = self.basesql + " AND flights.overVlbg=0"
-        cursor.execute(sql)
-        rs = cursor.fetchall()
-        # loop over all flights and check'em 
-        for record in rs:
-            flightid = record[0]
-            logging.log(logging.INFO, flightid)
-            sql = "SELECT longitude, latitude FROM flightdata WHERE flightid=%i" %flightid
-            logging.info(sql)
-            cursor2 = self.db.cursor()
-            cursor2.execute(sql)
-            rs2 = cursor2.fetchall()
-            # only use 10% of data!
-            c = 0
-            SKIP = 30
-            length = len(rs2)
-            SKIP = length / 5
-            coordinateinfo = ""     
-            logging.info(SKIP)
-            for data in rs2:
-                longitude= data[0]
-                latitude = data[1]
-                # damn, some flights have pretty strange GPS-info! define a value range
-                if latitude > 40 and latitude < 50 and longitude > 8 and longitude < 15: 
-                    if SKIP and c % SKIP == 0:
-                        coordinateinfo += "%f,%f,20000 \n" %(longitude, latitude)
-                    c += 1
-            cursor2.close()
-            if length > 50:
+        if 1 in overVlbg:
+            sql = self.basesql + " AND flights.overVlbg=1" 
+            cursor = self.db.cursor()
+            cursor.execute(sql)
+            rs = cursor.fetchall()
+            # loop over all flights and check'em 
+            placemark = ""
+            for record in rs:
+                flightid = record[0]
+                callsign = record[1]
+                hexident = record[2]
+                ts = record[3]
+          
+                logging.log(logging.INFO, flightid)
+                sql = "SELECT longitude, latitude FROM flightdata WHERE flightid=%i" %flightid
+                logging.info(sql)
+                cursor2 = self.db.cursor()
+                cursor2.execute(sql)
+                rs2 = cursor2.fetchall()
+                
+                # only use 10% of data!
+                c = 0
+                SKIP = 30
+                length = len(rs2)
+                SKIP = length / 25
+                coordinateinfo = ""     
+                clist = []
+                logging.info(SKIP)
+                for data in rs2:
+                    longitude= data[0]
+                    latitude = data[1]
+                    # damn, some flights have pretty strange GPS-info! define a value range
+                    if latitude > 40 and latitude < 50 and longitude > 8 and longitude < 15: 
+                        if c % SKIP == 0:
+                            coordinateinfo += "%f,%f,20000 \n" %(longitude, latitude)
+                            clist.append( (longitude, latitude) )
+                        c += 1
+                cursor2.close()
                 placemark += """<Placemark>
-      <name>-</name>
-      <description>-</description>
-      <styleUrl>#blue</styleUrl>
-      <LineString>
-        <extrude>0</extrude>
-        <tessellate>1</tessellate>
-        <altitudeMode>absolute</altitudeMode>
-        <coordinates>%s</coordinates>
-      </LineString>
-    </Placemark>""" %coordinateinfo
+          <name>-</name>
+          <description>-</description>
+          <styleUrl>#red</styleUrl>
+          <LineString>
+            <extrude>0</extrude>
+            <tessellate>1</tessellate>
+            <altitudeMode>absolute</altitudeMode>
+            <coordinates>%s</coordinates>
+          </LineString>
+        </Placemark>""" %coordinateinfo
             
-        cursor.close() 
+                # append location icon
+                long, lat = clist[len(clist)/2]
+                placemark += """<Placemark>
+                <name>flight: %s</name>
+                <description>aircraft: %s  spotted at: %s</description>
+                <styleUrl>#normalPlacemark</styleUrl>
+                <Point>
+                <coordinates>%f,%f,20000</coordinates>
+                </Point>
+                </Placemark>
+            """ % (callsign, hexident, ts, long, lat)
+            
+            cursor.close() 
+        
+        if 0 in overVlbg:
+            cursor = self.db.cursor()
+            sql = self.basesql + " AND flights.overVlbg=0"
+            cursor.execute(sql)
+            rs = cursor.fetchall()
+            # loop over all flights and check'em 
+            for record in rs:
+                flightid = record[0]
+                logging.log(logging.INFO, flightid)
+                sql = "SELECT longitude, latitude FROM flightdata WHERE flightid=%i" %flightid
+                logging.info(sql)
+                cursor2 = self.db.cursor()
+                cursor2.execute(sql)
+                rs2 = cursor2.fetchall()
+                # only use 10% of data!
+                c = 0
+                SKIP = 30
+                length = len(rs2)
+                SKIP = length / 5
+                coordinateinfo = ""     
+                logging.info(SKIP)
+                for data in rs2:
+                    longitude= data[0]
+                    latitude = data[1]
+                    # damn, some flights have pretty strange GPS-info! define a value range
+                    if latitude > 40 and latitude < 50 and longitude > 8 and longitude < 15: 
+                        if SKIP and c % SKIP == 0:
+                            coordinateinfo += "%f,%f,20000 \n" %(longitude, latitude)
+                        c += 1
+                cursor2.close()
+                if length > 50:
+                    placemark += """<Placemark>
+          <name>-</name>
+          <description>-</description>
+          <styleUrl>#blue</styleUrl>
+          <LineString>
+            <extrude>0</extrude>
+            <tessellate>1</tessellate>
+            <altitudeMode>absolute</altitudeMode>
+            <coordinates>%s</coordinates>
+          </LineString>
+        </Placemark>""" %coordinateinfo
+                
+            cursor.close() 
     
-        FILENAME = 'flights.kml'
         f = open(FILENAME, 'w')
         kml = """<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://earth.google.com/kml/2.1">
   <Document>
     <name>Paths</name>
-    <description>Examples of paths. Note that the tessellate tag is by default
-      set to 0. If you want to create tessellated lines, they must be authored
-      (or edited) directly in KML.</description>
+    <description>Flight Display</description>
     <Style id="red">
       <LineStyle>
         <color>7d0000ff</color>
