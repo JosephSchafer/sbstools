9c1da2727443041c43ff588f9b8a02d7330f7946
dominik.bartenstein <dominik.bartenstein@f0663f9a-1f2c-0410-911e-1bad8ebd410d>
2007-05-18 20:22:00 +0000

    wuhuu, what a dirty hack ;)

--- testing/createKml.py
+++ testing/createKml.py
@@ -1,12 +1,67 @@
 #!/usr/bin/python
 # Create kml-file from flightdata 
+# what a hack *bg*
 # Copyright (GPL) 2007 Dominik Bartenstein <db@wahuu.at>
 import MySQLdb
 import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
-FILENAME = 'flights.kml'
 overVlbg = (1,)
+
+class HTMLCreator:
+    ''' hacky thing to create html with google maps, flights and infos '''
+    def __init__(self):
+        self.flighttable = []
+    
+    def addFlight(self, callsign, hexident, date):
+        """ add flight to table """
+        self.flighttable.append( (callsign, hexident, date) )
+    
+    def createHTML(self, name):
+        # create HTML code for list entries
+        listfragment = ""
+        for callsign, hexident, date in self.flighttable:
+            airline = callsign[0:3]
+            number = callsign[3:]
+            date = str(date)[0:10]
+            listfragment += '<li><a href="http://www.flightstats.com/go/FlightStatus/flightStatusByFlight.do?airline=%s&flightNumber=%s&departureDate=%s">stats for %s </a></li>\n' % (airline, number, date, callsign)
+            
+        info = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <title>Google Maps Flightradar</title>
+    <script src="http://maps.google.com/maps?file=api&amp;v=2.x&amp;key=ABQIAAAA8E-yr4f8IpI_aBX8PhYOVxRuNWC2ipkI6dR5VCD3IuAM1IlVdBQcfokuIrZBiebzigmQ9wXbvXHGjQ"
+      type="text/javascript"></script>
+    <script type="text/javascript">
+
+var map;
+var geoXml = new GGeoXml("http://www.wahuu.at/~db/flights/kml/%s.kml");
+
+function load() {
+  if (GBrowserIsCompatible()) {
+    map = new GMap2(document.getElementById("map"));
+    map.addControl(new GLargeMapControl());
+    map.setCenter(new GLatLng(47.45753,9.96015), 3);
+    map.setZoom(10);
+
+    map.addOverlay(geoXml);
+  }
+}
+</script>
+</head>
+  <body onload="load()" onunload="GUnload()">
+    <div id="map" style="width: 1000px; height: 700px"></div>
+    <div>
+    <ul>
+        %s
+    </ul>
+    </div>
+  </body>
+</html>
+""" % (name, listfragment)
+        return info
 
 class KMLCreator:
     ''' creator of kml files '''
@@ -16,19 +71,28 @@ class KMLCreator:
     user = 'flight'
     password = 'flyaway'
  
-    def __init__(self):
+    def __init__(self, name):
         self.db = MySQLdb.connect(host = self.host, db = self.database, user = self.user, passwd = self.password)
         self.startdate = self.enddate = None
+        self.isPlacemarks = True
+        self.htmlcreator = HTMLCreator()
+        self.name = name
         
     def setLimits(self, startdate, enddate):
         ''' start- + enddate '''
         self.startdate = startdate
         self.enddate = enddate
     
+    def setIsPlacemarks(self, state):
+        ''' turn on/off placemarks for flights '''
+        self.isPlacemarks = state
+    
     def createFile(self):
         ''' create kml file! '''
         basesql = "SELECT distinct flights.id, callsign, aircrafts.hexident, flights.ts FROM flights LEFT JOIN flightdata ON flights.id=flightdata.flightid LEFT JOIN aircrafts ON flights.aircraftid=aircrafts.id WHERE ts BETWEEN '%s' AND '%s'" % (self.startdate, self.enddate)
         logging.info(basesql)
+        
+        # flights crossing Vorarlberg
         if 1 in overVlbg:
             sql = basesql + " AND flights.overVlbg=1" 
             cursor = self.db.cursor()
@@ -43,7 +107,7 @@ class KMLCreator:
                 ts = record[3]
           
                 logging.log(logging.INFO, flightid)
-                sql = "SELECT longitude, latitude, altitude FROM flightdata WHERE flightid=%i" %flightid
+                sql = "SELECT longitude, latitude, altitude, time FROM flightdata WHERE flightid=%i" %flightid
                 logging.info(sql)
                 cursor2 = self.db.cursor()
                 cursor2.execute(sql)
@@ -54,6 +118,9 @@ class KMLCreator:
                 SKIP = 30
                 length = len(rs2)
                 SKIP = length / 25
+                SKIP = 30
+                if SKIP == 0:
+                    SKIP = 1
                 coordinateinfo = ""     
                 clist = []
                 logging.info(SKIP)
@@ -61,16 +128,17 @@ class KMLCreator:
                     longitude= data[0]
                     latitude = data[1]
                     altitude = data[2]
+                    time = data[3]
                     # damn, some flights have pretty strange GPS-info! define a value range
                     if latitude > 40 and latitude < 50 and longitude > 8 and longitude < 15: 
                         if c % SKIP == 0:
                             coordinateinfo += "%f,%f,%d \n" %(longitude, latitude, altitude)
-                            clist.append( (longitude, latitude) )
+                            clist.append( (longitude, latitude, altitude, time) )
                         c += 1
                 cursor2.close()
                 placemark += """<Placemark>
-          <name>-</name>
           <description>-</description>
+          <name>-</name>
           <styleUrl>#red</styleUrl>
           <LineString>
             <extrude>0</extrude>
@@ -80,20 +148,41 @@ class KMLCreator:
           </LineString>
         </Placemark>""" %coordinateinfo
             
-                # append location icon
-                long, lat = clist[len(clist)/2]
-                placemark += """<Placemark>
-                <name>flight: %s</name>
-                <description>aircraft: %s  spotted at: %s</description>
-                <styleUrl>#normalPlacemark</styleUrl>
-                <Point>
-                <coordinates>%f,%f,20000</coordinates>
-                </Point>
-                </Placemark>
-            """ % (callsign, hexident, ts, long, lat)
-            
+                if self.isPlacemarks:
+                    # append location icon which is most close to longitude 9.6
+                    longitude = latitude = altitude = 0
+                    isAscending = 1
+                    # find out if flight flies W->E or E->W
+                    if len(clist) > 1:
+                        long1 = clist[0][0]
+                        long2 = clist[1][0]
+                        if long1 > long2:
+                            isAscending = 0
+                        
+                    for long, lat, alt, time in clist:
+                        longitude = long
+                        latitude = lat
+                        altitude = alt
+                        ts = time
+                        if isAscending == 1 and long > 9.7:
+                            break
+                        elif isAscending == 0 and long < 9.7:
+                            break
+                    #long, lat, alt = clist[len(clist)/2]
+                    long, lat, alt = longitude, latitude, altitude
+                    self.htmlcreator.addFlight( callsign, hexident, ts) 
+                    placemark += """\n<Placemark>
+                    <description>hexident: %s time: %s</description>
+                    <name>callsign: %s</name>
+                    <styleUrl>#normalPlacemark</styleUrl>
+                    <Point>
+                    <coordinates>%f,%f,%d</coordinates>
+                    </Point>
+                    </Placemark>
+                """ % (hexident, ts, callsign, long, lat, alt)
             cursor.close() 
         
+        # flights _NOT_ crossing Vorarlberg
         if 0 in overVlbg:
             cursor = self.db.cursor()
             sql = basesql + " AND flights.overVlbg=0"
@@ -140,7 +229,7 @@ class KMLCreator:
                 
             cursor.close() 
     
-        f = open(FILENAME, 'w')
+        f = open(self.name + ".kml", 'w')
         kml = """<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://earth.google.com/kml/2.1">
   <Document>
@@ -170,6 +259,9 @@ class KMLCreator:
           <href>http://www.wahuu.at/~db/flights/icon.png</href>
         </Icon>
       </IconStyle>
+      <LabelStyle>
+          <color>7dff0000</color>
+    </LabelStyle>
     </Style>
     <Folder>
     <description>flights</description>
@@ -178,13 +270,17 @@ class KMLCreator:
     %s
     </Folder>
   </Document>
-</kml>""" % placemark
+</kml>
+""" % placemark
         logging.log(logging.INFO, kml)
         f.write(kml)
         f.close()
         
+        HTMLFILENAME = self.name + ".html"
+        f = open(HTMLFILENAME, 'w')
+        f.write(self.htmlcreator.createHTML(self.name))
+        f.close()
         
-    
 def main():
     ''' kml creator '''
     
@@ -194,15 +290,17 @@ def main():
     parser = OptionParser()
     parser.add_option("-s", "--startdate", dest="startdate", help="startdate", metavar="STARTDATE")
     parser.add_option("-e", "--enddate", dest="enddate", help="enddate", metavar="ENDDATE")
+    parser.add_option("-n", "--name", dest="filename", help="filename", metavar="NAME")
+    parser.add_option("-p", "--placemarks", action="store_false", dest="isPlacemarks", default=True, help="add placemarks to each flight")
     options, args = parser.parse_args()
-   
     startdate = options.startdate
     enddate = options.enddate
-    if startdate == None or enddate == None:
+    if startdate == None or enddate == None or options.filename == None:
         logging.info("missing arguments")
         return
-    creator = KMLCreator()
+    creator = KMLCreator(options.filename)
     creator.setLimits(options.startdate, options.enddate)
+    creator.setIsPlacemarks(options.isPlacemarks)
     creator.createFile()
     logging.info("### KMLCreator finished")
  
