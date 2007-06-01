ecccfb4721c6f211edbc6f1b5bcdc3ac3e940c60
dominik.bartenstein <dominik.bartenstein@f0663f9a-1f2c-0410-911e-1bad8ebd410d>
2007-06-01 13:01:27 +0000

    - added section flightdatareducer to sbstools.cfg - cleaned up flightdatareducer and added SafeConfigParser

--- flightdatareducer.py
+++ flightdatareducer.py
@@ -1,27 +1,25 @@
-#!/usr/bin/python
-# Applying reduction filter to flights
-# i.e. reducing the amount of data, 
+# !/usr/bin/python
+# Reduction filter: reduce flight data
 # Copyright (GPL) 2007 Dominik Bartenstein <db@wahuu.at>
 import time
 import MySQLdb
 import logging
+from ConfigParser import SafeConfigParser
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
-class FlightdataFilter:
-    ''' filter data to reduce amount '''
+class FlightdataReducer:
+    ''' reduce flight data (tables: flightdata&airbornevelocitymessage) '''
     
-    # __FIXME__: should be stored in a separate configuration file
-    host = 'localhost'
-    database = 'flightdb'
-    user = 'flight'
-    password = 'flyaway'
+    def __init__(self, host, database, user, password):
+        self.db = MySQLdb.connect(host = host, db = database, user = user, passwd = password)
     
-    def __init__(self):
-        self.db = MySQLdb.connect(host = self.host, db = self.database, user = self.user, passwd = self.password)
-        
-    def runFilter(self, flightid):
-        ''' reduce amount of data '''
+    def setPercentage(self, percentage):
+        ''' percentage of data to be kept '''
+        self.percentage = percentage
+    
+    def reduceFlight(self, flightid):
+        ''' reduce flight data by deleting selected flightdata&airbornevelocitymessage-entries '''
         
         cursor = self.db.cursor()
         cursor.execute("SET AUTOCOMMIT=0")
@@ -30,23 +28,11 @@ class FlightdataFilter:
         # 1b. get ids for data to be removed from table airbornevelocitymessage
         # 2b. remove records
         # 3. update status of flight record in flights table 
-        flightdataids  = self.categorizeFlightdata(flightid, 'flightdata')
-        airbornevelocityids = self.categorizeFlightdata(flightid, 'airbornevelocitymessage')
-        # state:
-        # 0: not reduced
-        # 1: reduced
+        flightdataids  = self.getRemovableIds(flightid, 'flightdata')
+        airbornevelocityids = self.getRemovableIds(flightid, 'airbornevelocitymessage')
+        
+        # state 1 => reduced
         state = 1
-        
-        # if there are less than 50 records, do not reduce!
-        if len(flightdataids) < 50 or len(airbornevelocityids) < 50:
-            logging.info("length removable flightdata records: %i" %len(flightdataids))
-            logging.info("length removable airbornevelocitymessage records: %i" %len(airbornevelocityids))
-            logging.info("not reducing flight# %i" %flightid)
-           
-            flightdataids = airbornevelocityids = []
-            state = 0 # not reduced
-        logging.info("flight #%i" %flightid)
-        
         try:
             # begin transaction
             # don't care about performance for now: execute individual sql-statements
@@ -72,7 +58,7 @@ class FlightdataFilter:
             self.db.commit()
         cursor.close()
     
-    def categorizeFlightdata(self, flightid, table='flightdata'):
+    def getRemovableIds(self, flightid, table='flightdata'):
         ''' return ids to be deleted '''
         
         ids = []
@@ -85,17 +71,16 @@ class FlightdataFilter:
             id = record[0]
             ids.append(id)
         
-        # __FIXME__: a better way of reducing the dataflood would be to
+        # __IDEA__: a better way of reducing the dataflood would be to
         # consider the timestamp of the entries 
-        
         # loop over all ids and select those to delete
-        step = 20 # keep ~5% of the data
+        step = 100 / self.percentage
         dispensableids = []
         
         for i in range( len(ids) ):
             if i % step != 0:
                 dispensableids.append( ids[i] )
-        # ensure that the most recent entry for the particular flight is kept
+        # ensure that the most recent entry of the current flight is kept
         if len(ids) and ids[-1] in dispensableids:
             dispensableids.remove( ids[-1] )
         cursor.close()
@@ -114,25 +99,29 @@ class FlightdataFilter:
         return dispensableids
     
 def main():
-    logging.info("### FILTER started")
-    filter = FlightdataFilter()
+    logging.info("### REDUCER started")
+    cfg = SafeConfigParser()
+    cfg.read(sys.path[0] + os.sep + 'sbstools.cfg')
     
-    cursor = filter.db.cursor()
+    reducer = FlightdataReducer( cfg.get('db', 'host'), cfg.get('db', 'database'), cfg.get('db', 'user'), cfg.get('db', 'password') )
+    reducer.setPercentage( cfg.get('flightdatareducer', 'percentage') )
+    
+    cursor = reducer.db.cursor()
     # grab all flights, which:
-    # - are not yet classified/reduced (state IS NULL)
+    # - are not yet reduced (state IS NULL)
     # - did _not_ cross Vorarlberg (overVlbg=0)
     # - where the callsign flickering problem was solved (mergestate IS NOT NULL)
     sql = "SELECT id FROM flights WHERE state IS NULL AND overVlbg=0 AND mergestate IS NOT NULL"
     cursor.execute(sql)
     rs = cursor.fetchall()
     
-    # loop over all flights and check'em 
+    # loop over all flights and reduce'em
     for record in rs:
         flightid = record[0]
-        filter.runFilter(flightid)
+        reducer.reduceFlight(flightid)
 
     cursor.close() 
-    logging.info("### FILTER finished")
+    logging.info("### REDUCER finished")
  
 if __name__ == '__main__':
     main()
