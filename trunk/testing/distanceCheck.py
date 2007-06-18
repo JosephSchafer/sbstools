f2c03464f9fc7d7e5e210b2c3bc4c97e5dc5e1e6
dominik.bartenstein <dominik.bartenstein@f0663f9a-1f2c-0410-911e-1bad8ebd410d>
2007-06-18 16:33:06 +0000

    added formula for calculating distance between two gps points. special thanks for discussion gps related stuff to Benjamin and Janski from the beerclub

--- trunk/testing/distanceCheck.py
+++ trunk/testing/distanceCheck.py
@@ -1,14 +1,46 @@
-#!/usr/bin/python
+#!/uslr/bin/python
 # calculate distance between GPS-coordinates of flightdata infos 
 # what a hack *bg*
 # Copyright (GPL) 2007 Dominik Bartenstein <db@wahuu.at>
 import MySQLdb
 import logging
-import ogr
+import ogr, osr
 import sys, os
+import math
 from ConfigParser import SafeConfigParser
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+
+class DistanceCalc:
+    # based on code from by Aapo Rista (http://positio.rista.net/en/pys60gps/)
+    # http://mathforum.org/library/drmath/view/55417.html
+    # tc1=mod(atan2(sin(lon2-lon1)*cos(lat2),
+    #         cos(lat1)*sin(lat2)-sin(lat1)*cos(lat2)*cos(lon2-lon1)),
+    #         2*pi)
+    def rad2deg(self, rad):
+        """Convert radians to degrees.
+        Return float radians."""
+        return rad * 180 / math.pi
+    
+    def deg2rad(self, deg):
+        """Convert degrees to radians.
+        Return float degrees"""
+        return deg * math.pi / 180
+
+    def distance(self, lat1, lon1, lat2, lon2):
+        """Calculate dinstance between two lat/lon pairs.
+        Return float distance in meters."""
+        lat1 = self.deg2rad(lat1)
+        lon1 = self.deg2rad(lon1)
+        lat2 = self.deg2rad(lat2)
+        lon2 = self.deg2rad(lon2)
+        theta = lon1 - lon2
+        dist = math.sin(lat1) * math.sin(lat2) \
+             + math.cos(lat1) * math.cos(lat2) * math.cos(theta)
+        dist = math.acos(dist)
+        dist = self.rad2deg(dist)
+        meters = dist * 60 * 1852
+        return meters
 
 class DistanceChecker:
     ''' hacky thing to calculate distance between two points '''
@@ -31,32 +63,47 @@ class DistanceChecker:
     def checkFlight(self, flightid):
         ''' check GPS data of flight '''
         
+        spatref = osr.SpatialReference()
+        spatref.SetWellKnownGeogCS("WGS84")
+        distcalc = DistanceCalc()
+        
         logging.info("checking flight #%i" %flightid)
         points = []
         cursor = self.db.cursor()
-        sql = "SELECT latitude, longitude FROM flightdata WHERE flightid=%i" %flightid
+        sql = "SELECT latitude, longitude, UNIX_TIMESTAMP(time)*1000 + time_ms AS milliseconds FROM flightdata WHERE flightid=%i" %flightid
         cursor.execute(sql)
         rs = cursor.fetchall()
         # add all relevant flightdata to list!
         for record in rs:
             x = record[0]
             y = record[1]
-            points.append( (x, y) )
+            milliseconds = long(record[2])
+            points.append( (x, y, milliseconds) )
         cursor.close()    
     
         p = 0
-        
-        for x, y in points:
+        time = 0
+        for x, y, milliseconds in points:
             p2 = ogr.Geometry(ogr.wkbPoint)
+            p2.AssignSpatialReference(spatref)
             p2.SetPoint_2D(0, x, y)
+            time2 = milliseconds
             if p == 0:
                 p = p2
-            distance = p.Distance(p2)
-       
+                time = time2
+            distance = distcalc.distance( p.GetX(), p.GetY(), p2.GetX(), p2.GetY() )
+            timediff = time2 - time
+            try:
+                velocity = (3600 * 1000 / timediff) * distance / 1000
+            except:
+                velocity = -1
             # gotta convert distance into a readable format, e.g. km
-            if distance > 0.1:
+            if distance > 0:
                 logging.info( "\t%f between (%f, %f) and (%f, %f)" %(distance, p.GetX(), p.GetY(), p2.GetX(), p2.GetY()) )
+                logging.info( "\t%f milliseconds between these = %f kmph" % (timediff, velocity))
             p = p2
+            time = time2
+            #logging.info( p.GetSpatialReference() )
             
 def main():
     ''' distance checker '''
@@ -71,8 +118,8 @@ def main():
     dbpassword = cfg.get('db', 'password') 
     
     distancechecker = DistanceChecker(dbhost, dbname, dbuser, dbpassword)
-    distancechecker.checkAllFlights()
-    #distancechecker.checkFlight(127230)
+    #distancechecker.checkAllFlights()
+    distancechecker.checkFlight(101545)
     
     logging.info("### distance checker finished")
  
