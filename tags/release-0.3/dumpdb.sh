d7a71be6299e343fba8cbef3612902d81e1700c6
dominik.bartenstein <dominik.bartenstein@f0663f9a-1f2c-0410-911e-1bad8ebd410d>
2008-03-03 11:57:57 +0000

    compress sql dump on the fly

--- tags/release-0.3/dumpdb.sh
+++ tags/release-0.3/dumpdb.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # non-locking, ressource-friendly version of dumping the db
 # still, should not be run when production data is being collected
-mysqldump -u root -p flightdb --quick --skip-add-locks --skip-lock-tables > /tmp/flightdb.sql
+# mysqldump -u root -p flightdb --quick --skip-add-locks --skip-lock-tables > /tmp/flightdb.sql
+# 2008-03-03 compress sql-dump on the fly
+mysqldump -u root -p flightdb --quick --skip-add-locks --skip-lock-tables | bzip2 -c > /var/lib/mysql/flightdb-full-dump.bz2 
